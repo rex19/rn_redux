24c4b20dca2b16945366333588a76376f31d5ef3
Rex <rex_ni@outlook.com>
2017-12-16 22:53:31 +0800

    Initial commit

---
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/YGEnums.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.c \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h
---
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/YGNodeList.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
+  /Users/rex.ni/WS/ReactNative/rn01/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
